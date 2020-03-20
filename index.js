6634636339401482ae86305da334c56a497011d9
Sean Goedecke <sgoedecke@zendesk.com>
2020-03-20 13:24:07 +1100

    get PORT from env

--- index.js
+++ index.js
@@ -83,6 +83,6 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 3000, () => {
+  console.log('listening on *:', process.env.PORT || 3000);
 });
